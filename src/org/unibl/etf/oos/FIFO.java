f9a8c7af49680bf1208b83549606a89159eaa4b6
Djed01 <gordonletic5@gmail.com>
2022-05-21 09:25:57 +0200

    FIFO

--- src/org/unibl/etf/oos/FIFO.java
+++ src/org/unibl/etf/oos/FIFO.java
@@ -1,24 +1,78 @@
 package org.unibl.etf.oos;
 
-public abstract class Algoritam {
+public class FIFO {
     int brojOkvira;
     int brojReferenci;
     int nizReferenci[] = new int[brojReferenci];
-    int matrica[][] = new int[brojOkvira+2][brojReferenci];
+    String matrica[][];
 
-    Algoritam(){
-        super();
-    }
-    Algoritam(int brojOkvira,int brojReferenci,int []nizReferenci){
+    FIFO(int brojOkvira, int brojReferenci, int []nizReferenci){
         this.brojOkvira = brojOkvira;
         this.brojReferenci = brojReferenci;
         this.nizReferenci = nizReferenci;
+        this.matrica = new String[brojOkvira+2][brojReferenci];
     }
 
-    void upisiPrviRed(int matrica[][]){
+    void upisiPrviRed(){
         for(int i=0;i<brojReferenci;i++)
-            matrica[0][i]=nizReferenci[i];
+            matrica[0][i]=String.valueOf(nizReferenci[i]);
     }
+
+    boolean trebaUpisatiPF(int trenutnaKolona,int referenca){
+        if(trenutnaKolona==0){
+            return true;
+        }
+        for(int i=2;i<brojOkvira+2;i++){
+                if (String.valueOf(referenca).equals(matrica[i][trenutnaKolona - 1] )) {
+                    return false;
+                }
+            }
+
+        return true;
+    }
+
+
+    void ispisiMatricu( ){
+             for (int j = 0; j < brojOkvira + 2; j++)
+             {
+                 System.out.println("\n");
+                for(int i=0;i<brojReferenci;i++)
+                 System.out.printf("%3s",matrica[j][i]);
+             }
+    }
+
+    void popuniMatricu(){
+
+        for(int i=0;i<brojReferenci;i++)
+        {
+            if(trebaUpisatiPF(i,nizReferenci[i])){
+                matrica[1][i]="PF";
+            }
+            else{
+                matrica[1][i]="  ";
+            }
+            if(i==0){
+                matrica[2][i]=String.valueOf(nizReferenci[i]);
+                for(int j=3;j<brojOkvira+2;j++)
+                    matrica[j][i]="  ";
+            }
+            else{
+                if(matrica[1][i]=="PF"){
+                    matrica[2][i]=String.valueOf(nizReferenci[i]);
+                    for(int k = 3; k < brojOkvira+2; k++){
+                        matrica[k][i] = matrica[k-1][i-1];
+                    }
+                }
+                else {
+                    for(int k = 2; k<brojOkvira+2;k++){
+                        matrica[k][i] = matrica[k][i-1];
+                    }
+            }
+            }
+        }
+    }
+
+
 
 
 }
--- src/org/unibl/etf/oos/Main.java
+++ src/org/unibl/etf/oos/Main.java
@@ -1,8 +1,53 @@
 package org.unibl.etf.oos;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int n,r;
+        String tipAlgoritma;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Unesite broj okvira: ");
+        n = scanner.nextInt();
+        System.out.print("Unesite broj referenci:");
+        r = scanner.nextInt();
+
+        int nizReferecni[] = new int[r];
+        for(int i=0;i<r;i++){
+            System.out.print("Referenca["+(i+1)+"]: ");
+            nizReferecni[i] = scanner.nextInt();
+        }
+
+        do {
+            System.out.print("Izaberite algoritam:");
+            tipAlgoritma = scanner.nextLine();
+        } while (!tipAlgoritma.toUpperCase().equals("FIFO"));
+
+        switch (tipAlgoritma.toUpperCase()){
+            case "FIFO":
+            {
+                FIFO fifo = new FIFO(n,r,nizReferecni);
+                fifo.upisiPrviRed();
+                fifo.popuniMatricu();
+                fifo.ispisiMatricu();
+            }
+                break;
+            case "LRU":
+                //LRU ALGO
+                break;
+            case "SC":
+                //Secound chance
+                break;
+            case "LFU":
+                //LFU
+                break;
+            case "OPTIMALNI":
+                //Optimalni algoritam
+                break;
+            default:
+                System.out.println("EXCEPTION!");
+        }
+
     }
 }
